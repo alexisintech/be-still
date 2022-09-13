99505ece9b1f9889ea84076a7490334c06401d31
Alexis Aguilar <alexmerona@gmail.com>
2022-09-13 16:48:00 -0400

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+  return(
+    <header className="header">
+      <img className="header--logo" src="../imgs/logo.png" alt="" />
+      <h2 className="header--title">be still</h2>
+    </header>
+  )
+}
