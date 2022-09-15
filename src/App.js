5d070b3395d529e3e15323616ba8db606c26164f
Alexis Aguilar <alexmerona@gmail.com>
2022-09-15 17:00:51 -0400

    still component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Still from "./components/Still";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Still />
     </div>
   );
 }
---
+++ src/components/Still.js
@@ -0,0 +1,23 @@
+export default function Still(){
+  
+  return(
+    <main>
+      <section className="still--container">
+        <img src={`/imgs/be-still-1`} alt="x" className="still--image" />
+
+        <div className="form">
+          <input 
+            type="text"
+            placeholder="fade into you..."
+            className="form--input"
+          />
+          <button 
+            className="form--button"
+          >
+            Create new caption
+          </button>
+        </div>
+      </section>
+    </main>
+  )
+}
