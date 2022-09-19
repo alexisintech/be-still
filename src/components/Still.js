35d9420a84ee2043debaf0a9b0dea4ae25a4f473
Alexis Aguilar <alexmerona@gmail.com>
2022-09-19 17:04:59 -0400

    Update Still.js

--- src/components/Still.js
+++ src/components/Still.js
@@ -1,9 +1,16 @@
 export default function Still(){
-  
+  /*
+  Get random image from our array of images in still.js
+  */
+  function getStill(){
+    const randomStill = Math.floor(Math.random() * stills.length)
+    stillImage = stills[randomStill].img
+  }
+
   return(
     <main>
       <section className="still--container">
-        <img src={`/imgs/stills/be-still-1`} alt="x" className="still--image" />
+        <img src={`/imgs/stills/${stillImage}`} alt="x" className="still--image" />
 
         <div className="form">
           <input 
@@ -13,6 +20,7 @@ export default function Still(){
           />
           <button 
             className="form--button"
+            onClick={getStill}
           >
             Create new caption
           </button>
