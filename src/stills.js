2afb10d0849d9c9b1cd3ecb9b2bcf15882faa659
Alexis Aguilar <alexmerona@gmail.com>
2022-09-21 17:06:08 -0400

    Create stills.js

---
+++ src/stills.js
@@ -0,0 +1,34 @@
+exports.stills = [
+  {
+    id: 1,
+    img: "be-still-1.jpg",
+  },
+  {
+    id: 2,
+    img: "be-still-2.jpg",
+  },
+  {
+    id: 3,
+    img: "be-still-3.jpg",
+  },
+  {
+    id: 4,
+    img: "be-still-4.jpg",
+  },
+  {
+    id: 5,
+    img: "be-still-5.jpg",
+  },
+  {
+    id: 6,
+    img: "be-still-6.jpg",
+  },
+  {
+    id: 7,
+    img: "be-still-7.jpg",
+  },
+  {
+    id: 8,
+    img: "be-still-8.png",
+  },
+]
