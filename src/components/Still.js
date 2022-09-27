968ba466ce6e15c1ad97bba27534ff263f69a3e4
Alexis Aguilar <alexmerona@gmail.com>
2022-09-26 21:37:41 -0400

    reconfirguring state; styling

--- src/components/Still.js
+++ src/components/Still.js
@@ -2,27 +2,38 @@ import React, { useState, useEffect } from "react"
 import { stills } from "../stills";
 
 export default function Still(){
-  const [stillImage, setStillImage] = useState("be-still-3.jpg")
-
+  // const [stillImage, setStillImage] = useState("be-still-3.jpg")
+  const [still, setStill] = useState({
+    caption: "",
+    randomStill: "be-still-3.jpg"
+  })
   /*
   Get random image from our array of images in still.js
   */
   function getStill(){
-    const randomStill = Math.floor(Math.random() * stills.length)
-    setStillImage(stills[randomStill].img)
-    console.log(stillImage);
+    const randomNum = Math.floor(Math.random() * stills.length)
+    setStill(prevStill => ({
+      ...prevStill,
+      randomStill: stills[randomNum].img,
+    }))
   }
   
   return(
     <main>
       <section className="still--container">
-        <img src={`/imgs/stills/${stillImage}`} alt="x" className="still--image"/>
+        <div className="still">
+          <img src={`/imgs/stills/${still.randomStill}`} alt="x" className="still--image"/>
+          <h3 className="still--text">{still.caption}</h3>
+        </div>
 
         <div className="form">
           <input 
             type="text"
             placeholder="fade into you..."
             className="form--input"
+            name="caption"
+            value={still.caption}
+            onChange={updateCaption}
           />
           <button 
             className="form--button"
