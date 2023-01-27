c717e3837a9643754b87d31b423e1f2d1c54115e
Alexis Aguilar <alexmerona@gmail.com>
2023-01-26 19:35:47 -0500

    Update Still.js

--- src/components/Still.js
+++ src/components/Still.js
@@ -1,45 +1,48 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react";
 import { stills } from "../stills";
 
-export default function Still(){
-  
+export default function Still() {
   const [still, setStill] = useState({
     caption: "",
-    randomStill: "be-still-3.jpg"
-  })
+    randomStill: "be-still-3.jpg",
+  });
 
   /*
   As user types in form--input, still--text will be updated with the form--input value 
   */
-  function updateCaption(event){
-    const {name, value} = event.target
-    setStill(prevStill => ({
+  function updateCaption(event) {
+    const { name, value } = event.target;
+    setStill((prevStill) => ({
       ...prevStill,
-      [name]: value
-    }))
+      [name]: value,
+    }));
   }
 
   /*
   Get random image from our array of images in still.js
   */
-  function getStill(){
-    const randomNum = Math.floor(Math.random() * stills.length)
-    setStill(prevStill => ({
+  function getStill() {
+    const randomNum = Math.floor(Math.random() * stills.length);
+    setStill((prevStill) => ({
       ...prevStill,
       randomStill: stills[randomNum].img,
-    }))
+    }));
   }
-  
-  return(
+
+  return (
     <main>
       <section className="still--container">
         <div className="still" id="still">
-          <img src={`/imgs/stills/${still.randomStill}`} alt="x" className="still--image"/>
+          <img
+            src={`/imgs/stills/${still.randomStill}`}
+            alt="x"
+            className="still--image"
+          />
           <h3 className="still--text">{still.caption}</h3>
         </div>
 
         <div className="form">
-          <input 
+          <input
             type="text"
             placeholder="fade into you..."
             className="form--input"
@@ -47,14 +50,11 @@ export default function Still(){
             value={still.caption}
             onChange={updateCaption}
           />
-          <button 
-            className="form--button"
-            onClick={getStill}
-          >
+          <button className="form--button" onClick={getStill}>
             Generate new image
           </button>
         </div>
       </section>
     </main>
-  )
+  );
 }
